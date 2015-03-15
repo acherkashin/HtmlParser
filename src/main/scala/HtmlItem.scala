89e26efa203bf01ab6ff03b71e155ed5188ebd88
Alexandr Cherkashin <cherkashin_1996@mail.ru>
2015-03-16 01:12:41 +0300

    First Commit

---
+++ src/main/scala/HtmlItem.scala
@@ -0,0 +1,9 @@
+/**
+ * Created by Александр on 15.03.2015.
+ */
+class HtmlItem (val url: String, val description: String) {
+  def ToString(): String = s"url: $url\ndescription:$description\n"
+
+}
+
+
---
+++ src/main/scala/JSoupTest.scala
@@ -0,0 +1,11 @@
+/**
+ * Created by Александр on 15.03.2015.
+ */
+object JSoupTest {
+  def main(args: Array[String]): Unit = {
+    val htmlParser = new HtmlParser("http://habrahabr.ru/")
+    val htmlItems = htmlParser.LoadHtmlItem()
+    htmlItems.foreach(item => print(item.ToString))
+    print(htmlItems.length)
+  }
+}
